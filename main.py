da6f37e57b0b4de15bbd30e0ce5496f62c98d497
Andrewwangc2020 <Andrewwang.c2020@gmail.com>
2020-09-05 03:43:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+#Author: Andrew Wang aqw5628@psu.edu
+
+def letterToValue():
+  if lettergrade == "A":
+    return 4.0;
+  elif lettergrade == "A-":
+    return 3.67;
+  elif lettergrade == "B+":
+    return 3.33;
+  elif lettergrade == "B":
+    return 3.0;
+  elif lettergrade == "B-":
+    return 2.67;
+  elif lettergrade == "C+":
+    return 2.33;
+  elif lettergrade == "C":
+    return 2.0;
+  elif lettergrade == "D":
+    return 1.0;
+  else:
+    return 0.0;
+
+weight = 0.0;
+credits = 0.0;
+
+lettergrade = input("Enter your course 1 letter grade: ");
+l = letterToValue();
+c = input("Enter your course 1 credit: ");
+tempCredit = float(c);
+weight += l * tempCredit;
+credits += tempCredit;
+print(f"Grade point for course 1 is: {l}");
+
+lettergrade = input("Enter your course 2 letter grade: ");
+l = letterToValue();
+c = input("Enter your course 2 credit: ");
+tempCredit = float(c);
+weight += l * tempCredit;
+credits += tempCredit;
+print(f"Grade point for course 2 is: {l}");
+
+lettergrade = input("Enter your course 3 letter grade: ");
+l = letterToValue();
+c = input("Enter your course 3 credit: ");
+tempCredit = float(c);
+weight += l * tempCredit;
+credits += tempCredit;
+print(f"Grade point for course 3 is: {l}");
+
+gpa = weight/credits;
+print(f"Your GPA is: {gpa}");
